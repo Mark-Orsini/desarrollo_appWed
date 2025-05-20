8de3ea05e4aa4c5589d4b36a9e9d186cd2a5d5cd
Mark-Orsini <markorsini11@gmail.com>
2025-05-20 08:17:00 -0400

    Nueva

---
+++ JS/02_arrays/03_ejercicio/Ejercicios/script.js
@@ -0,0 +1,10 @@
+console.log("se conecto correctamente...")
+
+function mensajeText() {
+    let palabras = ["Mañana","Ayer","Hola","¿Que haces?","Vale","Ok","Ah","A"];
+    alert(palabras[2]+", "+palabras[3]+". "+palabras[0]+" voy "+palabras[7]+" Dormir "+palabras[4]+"?... "+palabras[5]);
+}
+
+function mensajeText2() {
+    let datos = [17, "Soy","Hola","Duermo","Temprano",""]
+}
