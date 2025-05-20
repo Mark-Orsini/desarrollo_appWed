a2bb02dbb300c8f7b489b6f05db48eb5c3fa2c11
Mark-Orsini <markorsini11@gmail.com>
2025-05-20 09:23:15 -0400

    Termine parte 2

--- JS/02_arrays/03_ejercicio/Ejercicios/script.js
+++ JS/02_arrays/03_ejercicio/Ejercicios/script.js
@@ -6,5 +6,36 @@ function mensajeText() {
 }
 
 function mensajeText2() {
-    let datos = [17, "Soy","Hola","Duermo","Temprano",""]
+    let datos = ["En "," La Granja "," rápidamente a través "," tres animales "," corren "," de la "," larga pradera"]
+    alert(datos[0]+datos[1]+" Los "+datos[3]+datos[4]+datos[2]+datos[5]+datos[6])
+}
+
+function matrizF() {
+    let matriz = [
+    [10, 20, 30],
+    [40, 50, 60],
+    [70, 80, 90]
+];
+    let resultado = matriz[0] [0]+matriz[2] [2]
+    alert("La suma de los numeros dentro de las matrizes es: "+ resultado)
+}
+
+function matriz3() {
+    let matriz = [
+    [1, 2, 3, 4],
+    [5, 6, [7, 8, [9, 10]]]
+];
+    let resultado = matriz[0] [1] + matriz[1] [2] [1] + matriz[1] [2] [2] [1]
+    alert("La suma de los numeros dentro de las matrizes es: "+ resultado)
+}
+
+function arregloMatriz() {
+    let sujeto = ["Perro","Gato","caiman"]
+    let acciones = [
+    ["ladra", "corre"],
+    ["duerme", "salta"],
+    ["Muerden", "Comen"],
+    ["Respirar", "Nadan"]
+];
+    alert("El "+sujeto[2]+" Hace: "+acciones[2] [0]+" Tambien "+acciones[2] [1]+" adivina saben "+acciones[3] [0]+" y "+acciones[3] [1])
 }
