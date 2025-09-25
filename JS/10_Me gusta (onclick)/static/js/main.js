0fdcbd8243c120477fc13146af6217802f17411a
Mark <markorsini11@gmail.com>
2025-09-25 19:20:13 -0300

    Casi terminado

--- JS/10_Me gusta (onclick)/static/js/main.js
+++ JS/10_Me gusta (onclick)/static/js/main.js
@@ -1,9 +1,19 @@
-function example(elemento) {
-    console.log("elemento clickedo", elemento);
+let YORN = false;
+
+function login() {
+    const loginBtn = document.getElementById('login-btn');
+    
+    if (YORN) {
+        loginBtn.textContent = 'Login';
+        YORN = false;
+    } else {
+        loginBtn.textContent = 'Logout';
+        YORN = true;
+    }
 }
 
-function turnOff(element) {
-    if (element.innerText == "On") {
-        
-    }
+function like(this) {
+    const elementoLike = document.getElementById(`btn-lik${this === 1 ? '' : this}`);
+    let currentLikes = parseInt(elementoLike.textContent);
+    elementoLike.textContent = currentLikes + 1;
 }
